ab1c096031de2218726490e9102dd8125522c754
rafaelqueiroz88 <rafael.castro6@fatec.sp.gov.br>
2018-02-08 17:53:36 -0200

    Algumas melhorias visuais
    Melhorias visuais na tela de chat

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ connections = [];
 server.listen(process.env.PORT || 3000);
 console.log('Server running...');
 
-
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
