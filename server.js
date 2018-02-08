906c604477390a39b18dd3203fb8a98f3e5c7269
rafaelqueiroz88 <rafael.castro6@fatec.sp.gov.br>
2018-02-08 17:04:39 -0200

    Estrutura
    Estrutura visual simples. Chat desenvolvido com socket.io e estrutura em nodejs.

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+users = [];
+connections = [];
+
+server.listen(process.env.PORT || 3000);
+console.log('Server running...');
+
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', function(socket){
+    connections.push(socket);
+    console.log('Connected: %s socket connected', connections.length);
+
+    //Disconnect
+    socket.on('disconnect', function(data){
+        connections.slice(connections.indexOf(socket), 1);
+        console.log('Disconnected: %s sockets connected', connections.length);
+    });
+
+    //Enviar mensagem
+    socket.on('send message', function(data){
+        console.log(data);
+        io.sockets.emit('new message', {msg: data})
+    });
+});
