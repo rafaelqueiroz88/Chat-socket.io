3c2a68d951d27fee90a3498ab0168c07691aeb79
rafaelqueiroz88 <rafael.castro6@fatec.sp.gov.br>
2018-02-15 15:40:04 -0200

    Limpeza de código
    Somente melhorias estéticas no código, nenhuma melhoria funcional

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('public'));
+
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 
